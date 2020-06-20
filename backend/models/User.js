7fb103c7ed0b8aa97a733b581087734ce89574df
SimonTheWang <simonwg2001@gmail.com>
2020-06-20 19:55:55 -0400

    updated user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose')
+
 const User = mongoose.Schema({
-    username: String,
-    password: String,
-    difficulty : String, default : 'Intermediate',
-    equipment : [String], default : ['Bodyweight'],
-    bodyPart : [String], default : ['All'],
-    workoutDuration : Number, default : 5,
+    username : String,
+    password : String,
+    difficulty : {type : String, default : 'Intermediate'},
+    equipment : {type : [String], default : ['Bodyweight']},
+    bodyPart : {type : [String], default : ['Abs','Back','Biceps','Chest','Forearm', 'Glutes', 'Shoulders', 'Triceps', 'Upper Legs', 'Lower Legs', 'Cardio']},
+    workoutDuration : {type : Number, default : 5},
     history:[{
         dates : Date,// must not include hours or minutes
-        workouts : [{
+        workouts : {type : [{
             time : [Date],//must include hours and minutes
             exercises : [String]
-        }], default : []
+        }], 
+        default : []}
     }],
-    blacklist:[String], default : []
+    blacklist:{default : [String], default : []}
 })
 
 module.exports = mongoose.model('User', User)
