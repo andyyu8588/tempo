2a3f95cc40588a4eb06fecd5ea5d9162316ff389
MoistWhale <andy.yu8588@gmail.com>
2020-06-20 19:49:19 -0400

    added http service

---
+++ app/src/app/services/http.service.ts
@@ -0,0 +1,76 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnDestroy } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// http service that gives httpclient to everything
+
+export class HttpService {
+  serverURL = environment.vision
+
+  constructor(private http: HttpClient) { }
+
+  delete(route: String, params: any) {
+    let serverRoute = this.serverURL + route
+    return new Promise((resolve, reject) => {
+      this.http.delete<any>(serverRoute, {
+        headers: {
+          authorization: localStorage.getItem('token') ? localStorage.getItem('token').toString() : 'monkas'
+        },
+        params
+      }).subscribe((res) => {
+        resolve(res)
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+
+  get(route: String, params: any) {
+    let serverRoute = this.serverURL + route
+    return new Promise((resolve, reject) => {
+      this.http.get<any>(serverRoute, {
+        headers: {
+          authorization: localStorage.getItem('token') ? localStorage.getItem('token').toString() : 'monkas'
+        },
+        params
+      }).subscribe((res) => {
+        resolve(res)
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+
+  post(route: String, params: any) {
+    let serverRoute = this.serverURL + route
+    return new Promise((resolve, reject) => {
+      this.http.post<any>(serverRoute,        
+        params
+      ).subscribe((res) => {
+        resolve(res)
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+
+  put(route: String, params: any) {
+    let serverRoute = this.serverURL + route
+    return new Promise((resolve, reject) => {
+      this.http.put<any>(serverRoute, {
+        headers: {
+          authorization: localStorage.getItem('token') ? localStorage.getItem('token').toString() : 'monkas'
+        },
+        params
+      }).subscribe((res) => {
+        resolve(res)
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+}
