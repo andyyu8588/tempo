ea1916fdac9dfe9d42261cdf64845efedeb13b4e
SimonTheWang <simonwg2001@gmail.com>
2020-06-21 00:35:20 -0400

    created router links and initialized sidebar

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnalysisComponent } from './components/analysis/analysis.component'
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WorkoutComponent } from './components/workout/workout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component : LoginComponent},
+  {path: 'register', component : RegisterComponent},
+  {path: 'analysis', component : AnalysisComponent},
+  {path: 'settings', component : SettingsComponent},
+  {path: 'workout', component : WorkoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
