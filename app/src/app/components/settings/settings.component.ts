3c63448c6a9f88c74aa851abd625571e466d37d4
ed99win <edwin99@live.ca>
2020-06-20 23:40:41 -0400

    added settings component

--- app/src/app/components/settings/settings.component.ts
+++ app/src/app/components/settings/settings.component.ts
@@ -1,3 +1,5 @@
+import { HttpResponse } from '@angular/common/http';
+import { HttpService } from './../../services/http.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  responseStatus: boolean = false
 
-  constructor() { }
+  selectedDifficulty: any = 'Intermediate'
+  muscleGroups: any[] = ['Abs','Back','Biceps','Chest','Forearm', 'Glutes', 'Shoulders', 'Triceps', 'Upper Legs', 'Lower Legs', 'Cardio']
+  private _muscleGroups: any[] = []
+
+  equipment: any[] = ['Bands', 'Barbell', 'Bench', 'Dumbbell', 'Exercise Ball', 'EZ - Bar', 'Foam Roll', 'Kettlebell', 'Machine - Cardio', 'Machine - Strength', 'Pull Bar', 'Weight Plate']
+  private _equipment: any[] = []
+
+  time: number = 10
+
+  formatLabel(value: number) {
+    this.time = value
+    if (value >= 60) {
+      return Math.round(value / 1);
+    }
+
+    return value;
+  }
+
+  constructor(private HttpService: HttpService) { }
 
   ngOnInit(): void {
+  }
+
+  changeMuscle(value: string) {
+    if (this._muscleGroups.includes(value)) {
+      this._muscleGroups.splice(this._muscleGroups.indexOf(value), 1)
+    } else {
+      this._muscleGroups.push(value)
+    }
+  }
+
+  changeEquip(value: string) {
+    if (this._equipment.includes(value)) {
+      this._equipment.splice(this._equipment.indexOf(value), 1)
+    } else {
+      this._equipment.push(value)
+    }
+  }
+
+  preferenceClick() {
+    console.log(this._muscleGroups)
+    this._equipment.push('Body Only')
+    // if (localStorage.getItem('username')) {
+      this.HttpService.post('/user/preferences', {
+        username: 'andy123',
+        difficulty: this.selectedDifficulty,
+        equipment: this._equipment,
+        bodyPart: this._muscleGroups,
+        workoutDuration: this.time,
+      })
+      .then((response: HttpResponse<any>) => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // } else {
+    //   console.log('no local')
+    // }
   }
 
 }
