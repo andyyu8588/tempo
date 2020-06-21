57d43fb3fc3356d25e615f2ae6fd20cbbb376b82
SimonTheWang <simonwg2001@gmail.com>
2020-06-20 21:27:21 -0400

    added new sidebar components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -44,11 +44,17 @@ import { MatTreeModule } from '@angular/material/tree';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AnalysisComponent } from './components/analysis/analysis.component';
+import { WorkoutComponent } from './components/workout/workout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SettingsComponent,
+    AnalysisComponent,
+    WorkoutComponent
   ],
   imports: [
     BrowserModule,
