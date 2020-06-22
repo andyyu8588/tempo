cd8da8c640744411c108d78b52ed18b36f1c1c97
ed99win <edwin99@live.ca>
2020-06-22 00:10:32 -0400

    ok

--- backend/app.js
+++ backend/app.js
@@ -23,14 +23,16 @@ app.use((req, res, next) => {
 
 app.use('/user', userRoute)
 
+app.use('/tempo.rar', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, '..', 'tempo.rar'))
+})
+
 // serve angular
 app.use('/:param',(req, res, next) => {
   res.sendFile(path.resolve(__dirname, '..', 'website', 'dist', 'website', req.params.param))
 })
 
-app.use('/tempo.rar', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, '..', 'tempo.rar'))
-})
+
 
 // set database URL:
 const dbURL = 'mongodb+srv://andy123:123@cluster0-esicd.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
