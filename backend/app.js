d9be8eb9753319498b562a8a31010362973a7a3b
ed99win <edwin99@live.ca>
2020-06-22 00:14:45 -0400

    ok

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 app.use('/user', userRoute)
 
 app.use('/tempo.rar', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, '..', 'tempo.rar'))
+  res.sendFile(path.resolve(__dirname, '..', 'tempo-win32-ia32'))
 })
 
 // serve angular
